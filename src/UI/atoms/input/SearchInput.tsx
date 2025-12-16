c773c444775c0d864999e9062a5b6770211f566f
daniel1775 <danilog1000@gmail.com>
2025-12-15 19:49:35 -0500

    feat: SearchInput created

---
+++ src/UI/atoms/input/SearchInput.tsx
@@ -0,0 +1,37 @@
+import { TextInput, View } from 'react-native';
+
+import SearchIcon from '@/assets/svg/Search';
+
+type TypeSearchInputProps = {
+	value: string;
+	setValue: (text: string) => void;
+	placeholder?: string;
+	containerStyle?: string;
+};
+
+const SearchInput = ({
+	value,
+	setValue,
+	placeholder,
+	containerStyle,
+}: TypeSearchInputProps) => {
+	return (
+		<View
+			className={`flex flex-row border-b border-orange w-full ${containerStyle}`}
+		>
+			<View className='mb-3'>
+				<SearchIcon />
+			</View>
+			<TextInput
+				className='ml-4 mb-2 text-[18px] text-light'
+				placeholder={placeholder}
+				onChangeText={(text) => {
+					setValue(text);
+				}}
+				value={value}
+			/>
+		</View>
+	);
+};
+
+export default SearchInput;
--- src/app/player/index.tsx
+++ src/app/player/index.tsx
@@ -1,14 +1,24 @@
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { View } from 'react-native';
 
+import SearchInput from '@/src/UI/atoms/input/SearchInput';
 import ScreenLayout from '@/src/UI/layouts/ScreenLayout';
 
 export default function Player() {
 	const { t } = useTranslation();
 
+	const [searchText, setSearchText] = useState('');
+
 	return (
 		<ScreenLayout title={t('players')}>
-			<View></View>
+			<View className='px-6'>
+				<SearchInput
+					value={searchText}
+					setValue={setSearchText}
+					containerStyle='mt-16'
+				/>
+			</View>
 		</ScreenLayout>
 	);
 }
