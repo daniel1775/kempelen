5aeed1944ed742a479a8b10382edbee3bd1fa90b
daniel1775 <danilog1000@gmail.com>
2026-01-07 18:55:55 -0500

    feat: PlayerCard init styles added

--- src/UI/molecules/player/PlayerCard.tsx
+++ src/UI/molecules/player/PlayerCard.tsx
@@ -1,13 +1,30 @@
-import { Image, View } from 'react-native';
+import { Image, ImageSourcePropType, Text, View } from 'react-native';
 
 type TypePlayerCardProps = {
-	imageUrl?: string;
+	playerName: string;
+	playerElo: number;
+	imageUrl?: ImageSourcePropType;
 };
 
-const PlayerCard = ({ imageUrl }: TypePlayerCardProps) => {
+const PlayerCard = ({
+	imageUrl,
+	playerName,
+	playerElo,
+}: TypePlayerCardProps) => {
 	return (
-		<View>
-			<Image />
+		<View className='flex flex-row bg-neutral-gray p-4'>
+			{imageUrl ? (
+				<Image
+					className='w-[76px] h-[76px]'
+					source={imageUrl}
+				/>
+			) : null}
+			<View className='ml-4'>
+				<Text className='text-light text-[15px] mb-2'>
+					{playerName}
+				</Text>
+				<Text className='text-light text-[15px]'>ELO: {playerElo}</Text>
+			</View>
 		</View>
 	);
 };
--- src/app/player/index.tsx
+++ src/app/player/index.tsx
@@ -2,8 +2,11 @@ import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { ScrollView, View } from 'react-native';
 
+import PlayerOneImage from '@/images/tests-images/player-1.png';
+import PlayerTwoImage from '@/images/tests-images/player-2.png';
 import SearchInput from '@/src/UI/atoms/input/SearchInput';
 import ScreenLayout from '@/src/UI/layouts/ScreenLayout';
+import PlayerCard from '@/src/UI/molecules/player/PlayerCard';
 
 export default function Player() {
 	const { t } = useTranslation();
@@ -16,9 +19,20 @@ export default function Player() {
 				<SearchInput
 					value={searchText}
 					setValue={setSearchText}
-					containerStyle='mt-16'
+					containerStyle='mt-16 mb-12'
 				/>
-				<ScrollView></ScrollView>
+				<ScrollView>
+					<PlayerCard
+						playerName='Magnus Carlsen'
+						playerElo={2862}
+						imageUrl={PlayerOneImage}
+					/>
+					<PlayerCard
+						playerName='Hikaru Nakamura'
+						playerElo={2787}
+						imageUrl={PlayerTwoImage}
+					/>
+				</ScrollView>
 			</View>
 		</ScreenLayout>
 	);
