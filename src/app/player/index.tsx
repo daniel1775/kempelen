dc812fc218c2e303778e76774f920b0c1b157a56
daniel1775 <danilog1000@gmail.com>
2026-01-07 21:22:26 -0500

    chore: declarations ts added

--- src/app/player/index.tsx
+++ src/app/player/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { ScrollView, View } from 'react-native';
+import { FlatList, View } from 'react-native';
 
 import PlayerOneImage from '@/images/tests-images/player-1.png';
 import PlayerTwoImage from '@/images/tests-images/player-2.png';
@@ -13,6 +13,19 @@ export default function Player() {
 
 	const [searchText, setSearchText] = useState('');
 
+	const dummyPlayers = [
+		{
+			playerName: 'Magnus Carlsen',
+			playerElo: 2862,
+			imageUrl: PlayerOneImage,
+		},
+		{
+			playerName: 'Hikaru Nakamura',
+			playerElo: 2787,
+			imageUrl: PlayerTwoImage,
+		},
+	];
+
 	return (
 		<ScreenLayout title={t('players')}>
 			<View className='px-6'>
@@ -21,18 +34,18 @@ export default function Player() {
 					setValue={setSearchText}
 					containerStyle='mt-16 mb-12'
 				/>
-				<ScrollView>
-					<PlayerCard
-						playerName='Magnus Carlsen'
-						playerElo={2862}
-						imageUrl={PlayerOneImage}
-					/>
-					<PlayerCard
-						playerName='Hikaru Nakamura'
-						playerElo={2787}
-						imageUrl={PlayerTwoImage}
-					/>
-				</ScrollView>
+				<FlatList
+					data={dummyPlayers}
+					keyExtractor={(_, index) => index.toString()}
+					renderItem={({ item }) => (
+						<PlayerCard
+							playerName={item.playerName}
+							playerElo={item.playerElo}
+							imageUrl={item.imageUrl}
+						/>
+					)}
+					ItemSeparatorComponent={() => <View className='h-6' />}
+				/>
 			</View>
 		</ScreenLayout>
 	);
