756e9d7e643a9ba8d4c806187ae0bf72b05862d7
daniel1775 <danilog1000@gmail.com>
2026-01-21 18:04:34 -0500

    chore: expo-image-picker permissions setup

--- src/UI/organisms/player/CreatePlayerForm.tsx
+++ src/UI/organisms/player/CreatePlayerForm.tsx
@@ -1,5 +1,6 @@
 import { useForm } from '@tanstack/react-form';
-import { Pressable, TextInput, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import { Alert, Pressable, TextInput, View } from 'react-native';
 
 import TextBase from '../../atoms/text/TextBase';
 
@@ -14,6 +15,30 @@ const CreatePlayerForm = () => {
 	});
 	const inputStyles =
 		'flex-1 text-[18px] border-b border-light-gray text-light-gray';
+
+	const pickImage = async () => {
+		const permissionResult =
+			await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+		if (!permissionResult.granted) {
+			Alert.alert(
+				'Permission required',
+				'Permission to access the media library is required.'
+			);
+			return;
+		}
+
+		let result = await ImagePicker.launchImageLibraryAsync({
+			mediaTypes: ['images'],
+			allowsEditing: true,
+			aspect: [4, 3],
+			quality: 1,
+		});
+
+		if (result.assets && result.assets.length > 0) {
+			form.setFieldValue('imageUri', result.assets[0].uri);
+		}
+	};
 
 	return (
 		<View className='gap-10 px-4 items-start'>
@@ -43,9 +68,12 @@ const CreatePlayerForm = () => {
 			</form.Field>
 			<form.Field name='imageUri'>
 				{(field) => (
-					<View className='flex-row gap-4 items-end'>
+					<View className='flex-row gap-4 items-start flex-1'>
 						<TextBase>Avatar: </TextBase>
-						<Pressable className=''>
+						<Pressable
+							onPress={pickImage}
+							className='border border-light-gray w-[240px] h-[240px] items-center justify-center'
+						>
 							<TextBase customStyles='text-orange underline'>
 								Upload Image
 							</TextBase>
