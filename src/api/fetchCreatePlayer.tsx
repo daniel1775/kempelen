e916ba57dce640312e8f67cab376857c230821e9
daniel1775 <danilog1000@gmail.com>
2026-02-08 11:00:25 -0500

    refactor(player): create player

--- src/api/fetchCreatePlayer.tsx
+++ src/api/fetchCreatePlayer.tsx
@@ -1,21 +1,26 @@
-import firestore from '@react-native-firebase/firestore';
+import {
+	getFirestore,
+	addDoc,
+	collection,
+} from '@react-native-firebase/firestore';
 import DeviceInfo from 'react-native-device-info';
 
 import type { TypePlayerToCreate } from '@/types/player';
 
 export const fetchCreatePlayer = async (playerToCreate: TypePlayerToCreate) => {
 	try {
+		const db = getFirestore();
 		const deviceId = await DeviceInfo.getUniqueId();
 
-		firestore()
-			.collection('players')
-			.add({
-				name: playerToCreate.name,
-				elo: playerToCreate.elo,
-				image_url: playerToCreate.imageUrl ?? '',
-				chess_profile: playerToCreate.chessProfileUrl ?? '',
-				device_id: deviceId,
-			});
+		const playerRef = collection(db, 'players');
+
+		await addDoc(playerRef, {
+			name: playerToCreate.name,
+			elo: playerToCreate.elo,
+			image_url: playerToCreate.imageUrl ?? '',
+			chess_profile: playerToCreate.chessProfileUrl ?? '',
+			device_id: deviceId,
+		});
 	} catch (err) {
 		console.error('[fetchCreatePlayer] error: ', err);
 	}
