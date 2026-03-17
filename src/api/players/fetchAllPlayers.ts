49b75341cc13f00ebcc7cce8ca80247135baf1f6
daniel1775 <danilog1000@gmail.com>
2026-03-17 15:01:03 -0500

    refactor(player): fetchAllPlayers migrated to new firebase api

--- src/api/players/fetchAllPlayers.ts
+++ src/api/players/fetchAllPlayers.ts
@@ -1,33 +1,34 @@
-import firestore from '@react-native-firebase/firestore';
+import {
+	collection,
+	getDocs,
+	getFirestore,
+	query,
+	where,
+} from '@react-native-firebase/firestore';
 import DeviceInfo from 'react-native-device-info';
 
 import type { TypePlayer } from '@/types/player';
 
 export const fetchAllPlayers = async () => {
 	try {
+		const db = getFirestore();
 		const deviceId = await DeviceInfo.getUniqueId();
 
-		const allPlayersRef = firestore().collection('players');
-		const snapshot = await allPlayersRef
-			.where('device_id', '==', deviceId)
-			.get();
+		const playersCollection = query(
+			collection(db, 'players'),
+			where('device_id', '==', deviceId),
+		);
+		const querySnapshot = await getDocs(playersCollection);
 
-		if (snapshot.empty) {
-			return [];
-		}
-
-		/* const prototype = Object.getPrototypeOf(snapshot);
-		console.log(Object.getOwnPropertyNames(prototype)); */
-
-		const allPlayers = snapshot.docs.map((doc) => {
+		const allPlayers = querySnapshot.docs.map((doc: any) => {
 			const docData = doc.data();
 
 			return {
 				id: doc.id,
-				name: docData.name,
-				chessProfile: docData.chess_profile,
-				elo: docData.elo,
-				imageUrl: docData.image_url,
+				name: docData?.name,
+				chessProfile: docData?.chess_profile,
+				elo: docData?.elo,
+				imageUrl: docData?.image_url,
 			};
 		});
 
