ec9114419f4c1f693e187ccec33af8c09986b985
daniel1775 <danilog1000@gmail.com>
2026-06-06 17:17:34 -0500

    fix(tournament): show image in single-tournament screen

--- src/UI/molecules/tournament/TournamentHeader.tsx
+++ src/UI/molecules/tournament/TournamentHeader.tsx
@@ -1,6 +1,8 @@
 import { View } from 'react-native';
 import { Image } from 'expo-image';
 
+import { resolveImageUri } from '@/src/utils/image/resolveImageUri';
+
 import DefaultTournamentImage from '@/images/tournament-default.jpg';
 
 type TypeTournamentHeaderProps = {
@@ -12,7 +14,7 @@ const TournamentHeader = ({ imageUrl }: TypeTournamentHeaderProps) => {
 		<View className='relative w-full h-64'>
 			{imageUrl ? (
 				<Image
-					source={{ uri: imageUrl }}
+					source={{ uri: resolveImageUri(imageUrl) }}
 					style={{ width: '100%', height: '100%' }}
 					contentFit='cover'
 				/>
