01c6dcb8edbe178073956fe32c80b8d6c96af5f3
daniel1775 <danilog1000@gmail.com>
2026-06-11 20:37:19 -0500

    feat: add variant to CustomButton

--- src/UI/atoms/buttons/CustomButton.tsx
+++ src/UI/atoms/buttons/CustomButton.tsx
@@ -4,24 +4,35 @@ import PlusIcon from '@/assets/svg/PlusIcon';
 
 type TypeCustomButtonProps = PressableProps & {
 	text?: string;
-	variant?: 'primary' | 'add';
+	variant?: 'primary' | 'add' | 'primary-sm';
 	buttonStyles?: string;
+	textStyles?: string;
 };
 
 const CustomButton = ({
 	text,
 	variant = 'primary',
 	buttonStyles = '',
+	textStyles = '',
 	...props
 }: TypeCustomButtonProps) => {
 	return (
 		<Pressable
 			{...props}
-			className={`bg-neutral-gray self-center ${variant === 'primary' ? 'rounded-full px-10 py-4' : 'rounded-full p-4 border border-orange'} ${buttonStyles}`}
+			className={`bg-neutral-gray self-center 
+				${buttonStyles}
+				${variant === 'primary-sm' && 'rounded-full px-8 py-3'}
+				${variant === 'primary' && 'rounded-full px-10 py-4'}
+				${variant === 'add' && 'rounded-full p-4 border border-orange'} 
+			`}
 		>
-			{variant === 'primary' && (
+			{(variant === 'primary' || variant === 'primary-sm') && (
 				<Text
-					className={`${variant === 'primary' ? 'text-orange' : 'text-black'} text-[18px] text-center`}
+					className={`text-orange text-center
+						${textStyles}
+						${variant === 'primary' && 'text-[16px]'} 
+						${variant === 'primary-sm' && 'text-[15px]'}
+					`}
 				>
 					{text}
 				</Text>
--- src/UI/organisms/tiebreak/TiebreakSortableList.tsx
+++ src/UI/organisms/tiebreak/TiebreakSortableList.tsx
@@ -1,5 +1,6 @@
 import { View, Modal } from 'react-native';
 import { useCallback } from 'react';
+import { useTranslation } from 'react-i18next';
 import {
 	Sortable,
 	SortableItem,
@@ -8,6 +9,7 @@ import {
 import TiebreakCard from '@/UI/molecules/tiebreak/TiebreakCard';
 
 import type { TypeTiebreak } from '@/src/types/tiebreak';
+import CustomButton from '@/UI/atoms/buttons/CustomButton';
 
 type TypeTiebreakSortableListProps = {
 	allTiebreaks: TypeTiebreak[];
@@ -16,6 +18,8 @@ type TypeTiebreakSortableListProps = {
 const TiebreakSortableList = ({
 	allTiebreaks,
 }: TypeTiebreakSortableListProps) => {
+	const { t } = useTranslation();
+
 	const renderItem = useCallback(
 		(props: SortableRenderItemProps<(typeof allTiebreaks)[0]>) => {
 			const { item, id, index, ...rest } = props;
@@ -45,13 +49,28 @@ const TiebreakSortableList = ({
 			transparent
 			animationType='fade'
 		>
-			<View className='flex-1 justify-center items-center bg-black/80 px-6'>
-				<View className=' h-[500px] w-[90%]'>
-					<Sortable
-						data={allTiebreaks}
-						renderItem={renderItem}
-						itemHeight={60}
-					/>
+			<View className='flex-1 justify-center items-center bg-black/80 px-6 '>
+				<View className='justify-center items-center w-[90%] bg-gray'>
+					<View className='h-[290px] w-[80%] bg-gray'>
+						<Sortable
+							data={allTiebreaks}
+							renderItem={renderItem}
+							itemHeight={60}
+							style={{ backgroundColor: '#23272A' }}
+						/>
+					</View>
+					<View className='flex-row justify-center gap-x-4 mt-6'>
+						<CustomButton
+							text={t('cancel')}
+							onPress={() => {}}
+							variant='primary-sm'
+						/>
+						<CustomButton
+							text={t('save')}
+							onPress={() => {}}
+							variant='primary-sm'
+						/>
+					</View>
 				</View>
 			</View>
 		</Modal>
